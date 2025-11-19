16f4c52e204c9f781619c5db656bb27c7bf62a43
Ing3567 <phongphol.suksomphong@g.swu.ac.th>
2025-11-19 19:31:03 +0700

    add EDA

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 from Clean import Clean_data, check_rain_in_evening
+from Visualize import plot_correlation, plot_hourly_rain_prob, plot_boxplot, plot_monthly_heatmap, plot_yearly_total , plot_scatter_custom
 
 def load_data(file_path):
     return pd.read_csv(file_path, skiprows=3)
@@ -24,7 +25,6 @@ def summary_data(data,groupy,type):
     return summary
 
 
-    
 
 
 if __name__ == "__main__":
@@ -33,6 +33,9 @@ if __name__ == "__main__":
     p.add_argument("--eveing", default=True, help="summary data" ,type=bool)
     p.add_argument("--group", default="year", help="group by column")
     p.add_argument("--type", default="mean", help="aggregation type: sum or mean")
+    p.add_argument("--plot", default="none", 
+                   choices=["none", "line", "heatmap", "bar", "corr", "box"], 
+                   help="Select plot type: line, heatmap, bar, corr")
     args = p.parse_args()
     file_path = args.file
     data = load_data(file_path)
@@ -42,9 +45,22 @@ if __name__ == "__main__":
     if args.eveing:
         rain_summary = check_rain_in_evening(data)
         print(rain_summary)
-
     else:
         summary = summary_data(data,args.group,args.type)
         print(summary)
+    if args.plot != "none":
+        print(f"\n--- Generating Plot: {args.plot} ---")
+        if args.plot == "line":
+            plot_hourly_rain_prob(data)
+        elif args.plot == "heatmap":
+            plot_monthly_heatmap(data)
+        elif args.plot == "bar":
+            plot_yearly_total(data)
+        elif args.plot == "corr":
+            plot_correlation(data)
+        elif args.plot == "box": 
+            plot_boxplot(data)
+        elif args.plot == "scat":
+            plot_scatter_custom(data)
 
     
